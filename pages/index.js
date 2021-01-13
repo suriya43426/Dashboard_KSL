ec436773985fa0b25eb8d97df930a40ec58c76d3
suriya chayatummagoon <6171006221@student.chula.ac.th>
2021-01-13 23:34:22 +0700

    Commit

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-        KSL_DASHBOARD <a href="https://nextjs.org">Next.js!</a>
+        KSL DASHBOARD <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
